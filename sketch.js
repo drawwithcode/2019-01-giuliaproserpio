c55c3da6cee0c162d6820e4c69467317f4eddc44
giuliaproserpio <giu.proserpio@gmail.com>
2019-09-21 21:58:36 +0200

    01_assignment

--- sketch.js
+++ sketch.js
@@ -3,9 +3,52 @@ function preload(){
 }
 
 function setup() {
-  // put setup code here
-}
+  createCanvas(windowWidth,windowHeight);
+  angleMode(DEGREES);
+  background("rosybrown");
+  frameRate(60);
+  }
 
 function draw() {
-  // put drawing code here
+  noFill();
+
+  stroke(
+      color("mistyrose"),
+      frameCount/360
+    );
+
+  translate(windowWidth/2,windowHeight/2);
+  line(cos(frameCount)*300, sin(frameCount)*300, 300, 0);
+
+  rotate(180)
+  line(cos(frameCount)*300, sin(frameCount)*300, 300, 0);
+
+  stroke(
+      color("lavander"),
+      frameCount/360
+  );
+  translate(100,0);
+  line(cos(frameCount)*200, sin(frameCount)*200, 200, 0);
+
+
+  stroke(
+      color("lightcyan"),
+      frameCount/360
+  );
+  translate(-200,0);
+  rotate(180)
+  line(cos(frameCount)*200, sin(frameCount)*200, 200, 0);
+
+  stroke(
+      color("aliceblue"),
+      frameCount/360
+  );
+  translate(-100,0);
+  rotate(180)
+  line(cos(frameCount)*100, sin(frameCount)*100, 100, 0);
+
+
+  if(frameCount == 360) {
+    noLoop();
+  }
 }
